67652338ecdd55491fa43d8e75ffe76f152aaa13
Mario Cosenza <61911701+mariocosenza@users.noreply.github.com>
2025-12-20 14:28:42 +0100

    feat(ai): implement send_push_notification_heartbeat

--- src/push_notification.py
+++ src/push_notification.py
@@ -7,11 +7,33 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(
 cred = credentials.Certificate("../data/account/serviceAccountKey.json")
 firebase_admin.initialize_app(cred)
 
-def send_push_notification(title, body):
+class LatestHeartbeat:
+    BPM: int = 0
+    NOTIED_FALL: bool = False
+
+async def send_push_notification(title, body):
     message = messaging.Message(
         notification=messaging.Notification(
             title=title,
             body=body,
+        ),
+         topic="fall",
+    )
+    messaging.send(messaging.Message(
+        data={
+            "action": "FALL_DETECTED",
+            "urgency": "high"
+        },
+        topic="fall",
+    ))
+    response = messaging.send(message)
+    logging.info(f"Successfully sent message: {response}")
+
+async def send_push_notification_heartbeat():
+    message = messaging.Message(
+        notification=messaging.Notification(
+            title="Man Fallen is not feeling well",
+            body="Man fallen heart rate: " + str(LatestHeartbeat.BPM) + "bpm",
         ),
          topic="fall",
     )
