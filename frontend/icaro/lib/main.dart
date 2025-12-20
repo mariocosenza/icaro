503f417cff31721ae057da033f861e2dda18cc16
Mario Cosenza <61911701+mariocosenza@users.noreply.github.com>
2025-12-20 14:27:39 +0100

    fix(flutter): typo

--- frontend/icaro/lib/main.dart
+++ frontend/icaro/lib/main.dart
@@ -69,7 +69,7 @@ class AlertsPage extends StatefulWidget {
 }
 
 class _AlertsPageState extends State<AlertsPage> {
-  static const String endpoint = "http://192.168.1.15:8000/api/v1/allerts";
+  static const String endpoint = "http://192.168.1.15:8000/api/v1/alerts";
 
   bool _loading = true;
   String? _error;
