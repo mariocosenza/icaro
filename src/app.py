dcb65c915b572f52c3fa3e6e679327163e390f09
Mario Cosenza <61911701+mariocosenza@users.noreply.github.com>
2025-12-20 17:23:03 +0100

    feat(ai): add param to change video quality

--- src/app.py
+++ src/app.py
@@ -6,7 +6,7 @@ from pose_landmark import main
 from push_notification import LatestHeartbeat
 
 app = FastAPI()
-main(vision.RunningMode.LIVE_STREAM)
+main(vision.RunningMode.LIVE_STREAM, quality="high")
 @app.get("/api/v1/alerts")
 def get_alerts():
     return get_all_data_from_mongo_db()
