88766413771b2b6a6cf9982876df2496dc8e135b
Mario Cosenza <61911701+mariocosenza@users.noreply.github.com>
2025-12-20 14:24:42 +0100

    feat(ai): post request for heartbeat

--- src/app.py
+++ src/app.py
@@ -1,11 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, logger
 from mediapipe.tasks.python import vision
 
 from mongodb import get_all_data_from_mongo_db
 from pose_landmark import main
+from push_notification import LatestHeartbeat
 
 app = FastAPI()
 main(vision.RunningMode.LIVE_STREAM)
 @app.get("/api/v1/alerts")
 def get_alerts():
     return get_all_data_from_mongo_db()
+
+@app.post("/api/v1/measure/{heartbeat}")
+def post_heartbeat(heartbeat: int):
+    LatestHeartbeat.BPM = heartbeat
+    logger.logger.log(level=logger.logger.INFO, msg=f"Heartbeat: {heartbeat}")
