ab1eb6fab214bf11af134e190fd7e98b79af8b07
Mario Cosenza <61911701+mariocosenza@users.noreply.github.com>
2025-12-20 14:28:19 +0100

    fix(ai): better exception handling

--- src/mongodb.py
+++ src/mongodb.py
@@ -1,10 +1,12 @@
+import logging
 import os
+import sys
 from pymongo import MongoClient
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-try:
-    CONNECTION_STRING = os.getenv("CONNECTION_STRING")
-except ImportError:
-    exit("CONNECTION_STRING not found in environment variables")
+CONNECTION_STRING = os.getenv("CONNECTION_STRING")
+if not CONNECTION_STRING:
+    sys.exit("CONNECTION_STRING not found in environment variables")
 
 
 def get_database():
@@ -12,18 +14,18 @@ def get_database():
     return client['icaro']
 
 
-def insert_message_mongo_db(title:str, message:str, alert:bool) -> None:
+async def insert_message_mongo_db(title: str, message: str, alert: bool) -> None:
     dbname = get_database()
-    print("Inserting aggregated data into MongoDB...")
-    collection_name = dbname["icaro"]
-    collection_name.insert({
+    logging.log(level=logging.INFO, msg='Inserting aggregated data into MongoDB...')
+    collection = dbname["icaro"]
+    collection.insert_one({
         'title': title,
         'message': message,
-        alert: alert
+        'alert': alert
     })
+
 
 def get_all_data_from_mongo_db() -> list:
     dbname = get_database()
-    collection_name = dbname["icaro"]
-    return list(collection_name.find())
-
+    collection = dbname["icaro"]
+    return list(collection.find())
